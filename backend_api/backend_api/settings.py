bf27e95089fd3c2030719940d34638db7afdc95b
my <knowurplace28@gmail.com>
2023-10-18 20:24:17 +0530

    dsds

--- backend_api/backend_api/settings.py
+++ backend_api/backend_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-hv*i1n9v8-z#e@81unw14%^@x%@ambj9dvdydd)1u%gi8at!to
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
